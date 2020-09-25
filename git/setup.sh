ca8f9e851f02e897f04e8b457a11b788fd961236
Jake Prime <jake.prime@gmail.com>
2020-09-25 11:35:47 +0100

    Add git .dotfiles

---
+++ git/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -s ~/dotfiles/git/git-coauthors ~/.git-coauthors
+ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/git/gitignore_global ~/.gitignore_global
+
+ln -s ~/dotfiles/git/gitconfig_futurelearn ~/work/futurelearn/gitconfig
+
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+~/dotfiles/git/setup.sh
+
