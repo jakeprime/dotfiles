9919ccdbd0da9b78cbe4fb9b9d0ae973ccc3aa26
Jake Prime <jake.prime@gmail.com>
2020-09-28 15:46:51 +0100

    Add vimrc

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 
 ~/.dotfiles/git/setup.sh
 ~/.dotfiles/ruby/setup.sh
+~/.dotfiles/vim/setup.sh
 ~/.dotfiles/zsh/setup.sh
---
+++ vim/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm ~/.vimrc
+ln -s ~/.dotfile/vim/vimrc ~/.vimrc
