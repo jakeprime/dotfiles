60dfde55d8e5bb0b15d1c61ad88117941c781544
Jake Prime <jake.prime@gmail.com>
2020-10-12 19:08:32 +0100

    Add z to zsh

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -8,4 +8,5 @@ cd fonts
 cd ..
 rm -rf fonts
 
+git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
