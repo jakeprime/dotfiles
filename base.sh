937aedef8ee5ba62e6e7ab255ef083305f0e46c7
jakeprime <jake.prime@gmail.com>
2021-04-13 16:38:58 +0100

    Add Goenv

--- base.sh
+++ base.sh
@@ -1,4 +1,5 @@
 source ~/.dotfiles/aliases.sh
+source ~/.dotfiles/goenv.sh
 source ~/.dotfiles/key-bindings.sh
 source ~/.dotfiles/nvm.sh
 source ~/.dotfiles/ruby.sh
--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@
 ~/.dotfiles/ruby/setup.sh
 ~/.dotfiles/vim/setup.sh
 ~/.dotfiles/zsh/setup.sh
+
+git clone https://github.com/syndbg/goenv.git ~/.goenv
