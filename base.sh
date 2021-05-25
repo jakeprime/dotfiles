caabcd4b1cd710936808b7123fca99f860208ce4
jakeprime <jake.prime@gmail.com>
2021-05-25 09:28:47 +0100

    Move secrets to first file loaded
    It will have envvars that other scripts may depend on

--- base.sh
+++ base.sh
@@ -1,8 +1,9 @@
+source ~/.dotfiles/secrets.sh
+
 source ~/.dotfiles/aliases.sh
 source ~/.dotfiles/goenv.sh
 source ~/.dotfiles/key-bindings.sh
 source ~/.dotfiles/nvm.sh
 source ~/.dotfiles/ruby.sh
 source ~/.dotfiles/scripts.sh
-source ~/.dotfiles/secrets.sh
 source ~/.dotfiles/zsh.sh
