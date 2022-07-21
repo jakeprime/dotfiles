30913b233530c73eebe783515722422ef01af647
jakeprime <jake.prime@gmail.com>
2022-07-21 14:13:13 +0100

    Update git commit running all CIs
    Will need to manually enter the branch_name in the rebase

--- aliases.sh
+++ aliases.sh
@@ -16,7 +16,7 @@ alias gadd="git add -N . && git add -p ."
 alias grim="git fetch && git rebase -i origin/master"
 alias grc="git rebase --continue"
 alias gra="git rebase --abort"
-alias gci="git rebase --keep-empty -i master -x \"git push --force-with-lease\""
+alias grimx="git fetch && git rebase --keep-empty -i origin HEAD:[branch_name] -x \"git push --force-with-lease\""
 gqrb() {
   git set-upstream
   git fetch origin "$@:$@" && git pull && git rebase "$@"
