304ea51ea6050e4e0e8ae8ba8bea7774768904ed
jakeprime <jake.prime@gmail.com>
2022-09-10 13:54:12 +0100

    Remove rails zsh package

--- zsh.sh
+++ zsh.sh
@@ -2,6 +2,6 @@ export ZSH="/Users/jake/.oh-my-zsh"
 
 ZSH_THEME="agnoster"
 
-plugins=(git extract brew rails sudo osx zsh-autosuggestions zsh-z)
+plugins=(git extract brew sudo macos zsh-autosuggestions zsh-z)
 
 source $ZSH/oh-my-zsh.sh
