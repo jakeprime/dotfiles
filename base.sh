fa4b899a35cc97860f98f278e1cf8015e0f909c6
jakeprime <jake.prime@gmail.com>
2022-09-10 13:48:01 +0100

    Add pyenv

--- base.sh
+++ base.sh
@@ -1,9 +1,11 @@
 source ~/.dotfiles/secrets.sh
 
 source ~/.dotfiles/aliases.sh
+source ~/.dotfiles/env.sh
 source ~/.dotfiles/goenv.sh
 source ~/.dotfiles/key-bindings.sh
 source ~/.dotfiles/nvm.sh
+source ~/.dotfiles/python.sh
 source ~/.dotfiles/ruby.sh
 source ~/.dotfiles/scripts.sh
 source ~/.dotfiles/zsh.sh
