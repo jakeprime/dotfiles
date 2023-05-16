fbf5e4e0d7c7b14bd2b0db4b91dc39340582b324
Jake Prime <jake@jakeprime.com>
2023-05-16 14:17:42 +0100

    Add code folding for Ruby
    I cannot believe this isn't built in to the Ruby mode!

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -589,6 +589,18 @@ before packages are loaded."
   (global-git-commit-mode t)
 
   (defalias 'forward-evil-word 'forward-evil-symbol)
+
+  ;; Ruby code folding
+  (add-hook 'ruby-mode-hook
+            (lambda () (hs-minor-mode)))
+
+  (eval-after-load "hideshow"
+    '(add-to-list 'hs-special-modes-alist
+                  `(ruby-mode
+                    ,(rx (or "def" "class" "module" "do" "{" "[")) ; Block start
+                    ,(rx (or "}" "]" "end"))                       ; Block end
+                    ,(rx (or "#" "=begin"))                        ; Comment start
+                    ruby-forward-sexp nil)))
 )
 
 
