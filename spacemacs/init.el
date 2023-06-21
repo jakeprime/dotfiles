373b896e6ea787dae50706403f792f3281003089
Jake Prime <jake@jakeprime.com>
2023-06-21 23:40:30 +0100

    Add custom vaporwave logo

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -191,7 +191,7 @@ It should only modify the values of Spacemacs settings."
    ;; directory. A string value must be a path to an image format supported
    ;; by your Emacs build.
    ;; If the value is nil then no banner is displayed. (default 'official)
-   dotspacemacs-startup-banner 'official
+   dotspacemacs-startup-banner "~/.spacemacs.d/vaporwave-sun.png"
 
    ;; Scale factor controls the scaling (size) of the startup banner. Default
    ;; value is `auto' for scaling the logo automatically to fit all buffer
