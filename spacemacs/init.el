948f35f114a97e922a93cfd4a8862ce14c4d8bff
Jake Prime <jake@jakeprime.com>
2023-11-06 01:55:31 +0000

    More bracket fiddling
    This seems to work now, and the parens stay the right colours.

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -478,7 +478,7 @@ It should only modify the values of Spacemacs settings."
 
    ;; If non-nil smartparens-mode will be enabled in programming modes.
    ;; (default t)
-   dotspacemacs-activate-smartparens-mode t
+   dotspacemacs-activate-smartparens-mode nil
 
    ;; If non-nil pressing the closing parenthesis `)' key in insert mode passes
    ;; over any automatically added closing parenthesis, bracket, quote, etc...
