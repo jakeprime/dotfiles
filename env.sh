e6f38ab17900e1c27421c2cf6d2f309142764cd8
Jake Prime <jake@jakeprime.com>
2024-04-13 11:52:42 +0100

    Skip timeout for Cleo tests
    When I put a binding.pry in I want it to stay there until I'm done.

--- env.sh
+++ env.sh
@@ -2,3 +2,4 @@ export EDITOR=vi
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 export EMACS=/Applications/Emacs.app/Contents/MacOS/emacs
 export NO_WEBPACK_COMPILE_CHECK=1
+export SKIP_TEST_TIMEOUT=1
