0ef2454ca9d58fe5251de67ffa90b4147ee6b855
Jake Prime <jake@jakeprime.com>
2024-05-20 02:44:13 +0100

    Save full shell history

--- env.sh
+++ env.sh
@@ -3,3 +3,6 @@ export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 export EMACS=/Applications/Emacs.app/Contents/MacOS/emacs
 export NO_WEBPACK_COMPILE_CHECK=1
 export SKIP_TEST_TIMEOUT=1
+export SAVEHIST=1000000
+export HISTFILE=~/.zsh_history
+setopt APPEND_HISTORY
