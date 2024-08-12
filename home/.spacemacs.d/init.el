38dd8b9db9eb36e5b67551e9da2c9221b80a7807
Jake Prime <jake@jakeprime.com>
2024-08-12 22:42:50 +0000

    Enable mu4e emails on linux
    - Add .mbsyncrc to homesick
    - Restore mu4e config to emacs

--- home/.spacemacs.d/init.el
+++ home/.spacemacs.d/init.el
@@ -50,6 +50,12 @@ This function should only modify configuration layer settings."
      json
      lsp
      markdown
+     (mu4e :variables
+           mu4e-installation-path "/usr/share/emacs/site-lisp/mu4e"
+           mu4e-get-mail-command "mbsync -a"
+           mu4e-update-interval (* 5 60)
+           mu4e-change-filenames-when-moving t
+           )
      multiple-cursors
      (org :variables
           org-agenda-files '("~/Org/Tasks.org" "~/Org/Journal/")
@@ -718,6 +724,67 @@ before packages are loaded."
         smtpmail-smtp-server "smtp.gmail.com"
         smtpmail-smtp-service 587
         smtpmail-debug-info t)
+
+  (with-eval-after-load 'mu4e
+    (setq mu4e-contexts
+          (list
+           (make-mu4e-context
+            :name "Cleo"
+            :match-func
+            (lambda (msg)
+              (when msg
+                (string-prefix-p "/cleo" (mu4e-message-field msg :maildir))))
+            :vars '((user-mail-address . "jake@meetcleo.com")
+                    (user-full-name . "Jake Prime")
+                    (mu4e-refile-folder . "/cleo/[Gmail]/All Mail")
+                    (mu4e-sent-folder . "/cleo/[Gmail]/Sent Mail")
+                    (mu4e-trash-folder . "/cleo/[Gmail]/Trash")
+                    (mu4e-alert-interesting-mail-query . "flag:unread AND maildir:/cleo/Inbox")
+                    (smtpmail-smtp-user . "jake@meetcleo.com")))
+           (make-mu4e-context
+            :name "Personal"
+            :match-func
+            (lambda (msg)
+              (when msg
+                (string-prefix-p "/personal" (mu4e-message-field msg :maildir))))
+            :vars '((user-mail-address . "jake@jakeprime.com")
+                    (user-full-name . "Jake Prime")
+                    (mu4e-refile-folder . "/personal/[Google Mail]/All Mail")
+                    (mu4e-sent-folder . "/personal/[Google Mail]/Sent Mail")
+                    (mu4e-trash-folder . "/personal/[Google Mail]/Bin")
+                    (mu4e-alert-interesting-mail-query . "flag:unread AND maildir:/personal/Inbox")
+                    (smtpmail-smtp-user . "jake.prime@gmail.com")))
+           ))
+    (add-to-list 'mu4e-marks
+                 '(trash
+                   :char ("d" . "▼")
+                   :prompt "dtrash"
+                   :dyn-target (lambda (target msg) (mu4e-get-trash-folder msg))
+                   :action (lambda (docid msg target)
+                             (mu4e--server-move docid
+                                                (mu4e--mark-check-target target) "+S-N"))))
+    (add-to-list 'mu4e-marks
+                 '(flag
+                   :char ("+" . "★")
+                   :prompt "dflag"
+                   :dyn-target (lambda (target msg)
+                                 (replace-regexp-in-string "All Mail" "Starred"
+                                                           (mu4e-get-refile-folder msg)))
+                   :action (lambda (docid msg target)
+                             (mu4e--server-move docid
+                                                (mu4e--mark-check-target target))))))
+
+  (setq mu4e-maildir-shortcuts
+        '((:maildir "/cleo/Inbox" :key ?c :name "Cleo" :hide t)
+          (:maildir "/personal/Inbox" :key ?p :name "Personal" :hide t)))
+  (setq mu4e-headers-attach-mark '("a" . "+"))
+  (setq mu4e-headers-list-mark '("l" . "@"))
+  (setq mu4e-headers-personal-mark '("p" . "."))
+  (setq mu4e-headers-flagged-mark '("f" . "!"))
+  (setq mu4e-headers-new-mark '("N" . "*"))
+
+  (add-hook 'mu4e-index-updated-hook #'mu4e-alert-enable-mode-line-display)
+
 
   (add-hook 'dired-mode-hook 'diredfl-mode)
 
