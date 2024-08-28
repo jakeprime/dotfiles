3d2f66141aa63fe8db0cd952aac64486de17b236
Jake Prime <jake@jakeprime.com>
2024-08-28 19:01:58 +0100

    Tell LSP to ignore these folders
    There are still literlly 1,000s of folders to watch in the Cleo projects, but we
    certainly don't need these ones in the mix.

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -649,6 +649,13 @@ before packages are loaded."
   (require 'asdf)
   (asdf-enable)
 
+  (with-eval-after-load 'lsp-mode
+    (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\].bundle\\'")
+    (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]tmp\\'")
+    (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\.jest-tmp\\'")
+    (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\.expo\\'")
+    (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]Pods\\'"))
+
   (setq magit-repository-directories
     '(("~/work/" . 2)))
 
