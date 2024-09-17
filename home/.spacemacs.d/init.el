e4a2f9810bfc5bea158b5e1e99a5366a1017c7b8
Jake Prime <jake@jakeprime.com>
2024-09-17 23:10:44 +0100

    Fix need for double loading config
    Until now I have always had to reload the init file on load for my preferred
    font to be loaded and rendered. This hack has sorted it.
    
    1. Tell Spacemacs to set the detault font to `nil`.
       I am not sure why it fails when attempting Monaspice, but this is actually the
       last thing to be run, and even when the correct font was being rendered during
       startup that final command would reset it to Source Code.
    2. Load the theme file twice.
       I have absolutely no idea why this is necessary, but it is.
    
    Unfortunately this does result in a warning on the home buffer, but better that
    than having to reload the init.

--- home/.spacemacs.d/init.el
+++ home/.spacemacs.d/init.el
@@ -262,10 +262,7 @@ It should only modify the values of Spacemacs settings."
    ;; fixed-pitch faces. The `:size' can be specified as
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
-   dotspacemacs-default-font '("Source Code Pro"
-                               :size 10.0
-                               :weight normal
-                               :width normal)
+   dotspacemacs-default-font '()
 
    ;; The leader key (default "SPC")
    dotspacemacs-leader-key "SPC"
