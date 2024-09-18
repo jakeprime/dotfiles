74638cbff212c7e3d53c2aa24bfca0dfa2f75075
Jake Prime <jake@jakeprime.com>
2024-09-18 21:03:36 +0100

    Save tangled files in the local directory
    This means the whole thing can be loaded on a new installation, rather
    than having to make sure that org is working first.

---
+++ home/.spacemacs.d/user-config.el
@@ -0,0 +1,38 @@
+(add-to-list 'custom-theme-load-path "~/.spacemacs.d/")
+(load-file "~/.spacemacs.d/faces-init.el")
+
+(setq dotspacemacs-startup-banner "~/.spacemacs.d/vaporwave-sun.png")
+
+(setq lsp-rubocop-use-bundler t)
+(setq lsp-ruby-lsp-use-bundler t)
+(setq lsp-solargraph-use-bundler t)
+
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (setq-local flycheck-command-wrapper-function
+                        (lambda (command) (append '("bundle" "exec") command)))))
+
+(assq-delete-all 'ruby-Test::Unit compilation-error-regexp-alist-alist)
+(add-to-list 'compilation-error-regexp-alist-alist '(ruby-Test::Unit "^ +\\([^ (].*\\):\\([1-9][0-9]*\\):in " 1 2))
+(assoc 'ruby-Test::Unit compilation-error-regexp-alist-alist)
+
+(setq evil-escape-key-sequence [106 107])
+
+(setq vc-follow-symlinks t)
+
+(use-package transient-posframe
+  :ensure t
+  :init (transient-posframe-mode))
+
+(defun my-org-mode-hook ()
+  (auto-fill-mode 0)
+  (face-remap-add-relative 'hl-line `(:background nil))
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-fill-column-mode 1)
+  (visual-line-mode 1)
+  (setq evil-auto-indent nil
+        visual-fill-column-width 120
+        visual-fill-column-center-text t))
+
+(add-hook 'org-mode-hook 'my-org-mode-hook)
