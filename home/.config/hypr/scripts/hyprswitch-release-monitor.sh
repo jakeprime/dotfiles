4ac1ac62d5f29e89b1f8214f45f14029a79076e6
Jake Prime <jake@jakeprime.com>
2024-09-29 14:43:59 +0100

    Detect both Supers
    On split keyboard I have to sometimes use the right and sometimes the left.

--- home/.config/hypr/scripts/hyprswitch-release-monitor.sh
+++ home/.config/hypr/scripts/hyprswitch-release-monitor.sh
@@ -5,7 +5,7 @@ libinput debug-events | while read -r line; do
     # Example: Check for key press events
     if [[ "$line" =~ "KEYBOARD_KEY" ]]; then
         if [[ "$line" =~ "released" ]]; then
-            if [[ "$line" =~ "KEY_LEFTMETA" ]]; then
+            if [[ "$line" =~ "KEY_LEFTMETA" || "$line" =~ "KEY_RIGHTMETA" ]]; then
                 hyprswitch close
                 hyprctl dispatch submap reset
             fi
