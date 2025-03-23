4ead057a302a1b550f80e07c2a3bfff4ac4a23be
Jake Prime <jake@jakeprime.com>
2025-03-23 17:40:24 +0000

    Remove Sorbet as a default
    It doesn't like it if there aren't all the Sorbet config files in the
    project. Better to default to not running it and then enable via
    dir-local variables.

--- home/.spacemacs.d/user-config.el
+++ home/.spacemacs.d/user-config.el
@@ -84,8 +84,7 @@
                  (lambda (command)
 (append (list (concat (project-root (project-current)) "bin/bundle") "exec") command)))))
 
-(setq lsp-disabled-clients '(rubocop-ls ruby-ls))
-(setq lsp-sorbet-as-add-on t)
+(setq lsp-disabled-clients '(rubocop-ls ruby-ls sorbet-ls))
 
 (assq-delete-all 'ruby-Test::Unit compilation-error-regexp-alist-alist)
 (add-to-list 'compilation-error-regexp-alist-alist '(ruby-Test::Unit "^ +\\([^ (].*\\):\\([1-9][0-9]*\\):in " 1 2))
