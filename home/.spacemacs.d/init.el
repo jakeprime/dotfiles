52c9fef3bef2e82ffe81be870a8ff519990d2f38
Jake Prime <jake@jakeprime.com>
2025-05-12 21:47:29 +0100

    Remove custom var set in init.el
    I have no idea what this is.

--- home/.spacemacs.d/init.el
+++ home/.spacemacs.d/init.el
@@ -597,7 +597,6 @@ This function is called at the very end of Spacemacs initialization."
    ;; If you edit it by hand, you could mess it up, so be careful.
    ;; Your init file should contain only one such instance.
    ;; If there is more than one, they won't work right.
-   '(package-selected-packages nil)
    '(safe-local-variable-values
      '((lsp-sorbet-use-bundler . t) (lsp-sorbet-as-add-on . t)
        (typescript-backend . tide) (typescript-backend . lsp)
