835cd6540fef6c51c268dda3a603b8f8917280ff
Jake Prime <jake@jakeprime.com>
2026-08-14 10:54:47 +0100

    Specify meetcleo as a Rails app for projectile
    The projectile checker is pretty dump and decides that it is a Julia
    project because there is a "project.toml" file in the root, and that
    check just happens to be executed before the check for a "Gemfile".

--- home/.config/jake/dir-locals/meetcleo--meetcleo.el
+++ home/.config/jake/dir-locals/meetcleo--meetcleo.el
@@ -1,4 +1,5 @@
 ((ruby-mode . ((lsp-sorbet-use-bundler . t)
                (lsp-sorbet-as-add-on . t)
                (lsp-disabled-clients . (rubocop-ls ruby-ls))
+               (projectile-project-type . rails-test)
                (ruby-test-rails-test-command . "bin/spring rails test"))))
